4a47ec7d215575832ab47aaa987309e961a3fba9
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-08-16 18:00:21 +0300

    fix delete

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,7 @@ import exman
 
 
 def test_mark(parser: exman.ExParser, script_runner, root):
+    script_runner.launch_mode = 'in_process'
     args = parser.parse_args([])
     info = script_runner.run('exman', 'mark', 'new', '1', '2', cwd=root)
     assert info.success
@@ -13,6 +14,7 @@ def test_mark(parser: exman.ExParser, script_runner, root):
 
 
 def test_delete(parser: exman.ExParser, script_runner, root):
+    script_runner.launch_mode = 'in_process'
     args = parser.parse_args([])
     assert (parser.runs / args.root.name).exists()
     assert (parser.index / exman.parser.yaml_file(args.root.name)).exists()
@@ -21,6 +23,13 @@ def test_delete(parser: exman.ExParser, script_runner, root):
     assert r'runs {2} were not found' in info.stderr
     assert not (parser.index / exman.parser.yaml_file(args.root.name)).exists()
     assert (parser.runs / args.root.name).exists()
+
+
+def test_delete_all(parser: exman.ExParser, script_runner, root):
+    script_runner.launch_mode = 'in_process'
+    args = parser.parse_args([])
+    assert (parser.runs / args.root.name).exists()
+    assert (parser.index / exman.parser.yaml_file(args.root.name)).exists()
     info1 = script_runner.run('exman', 'delete', '--all', '1', '2', cwd=root)
     assert info1.success
     assert r'runs {2} were not found' in info1.stderr
